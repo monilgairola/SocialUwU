e39169f119313ecc0b1e996a63d56633b4cc151d
Varun <lanjharav@gmail.com>
2022-01-31 04:07:22 +0530

    Home page stuff done adn scrollbar stuff also

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -6,7 +6,7 @@ const Feed = () => {
     <div className="feed">
       <div className="header">
         <div className="user">
-          <div className="pfp">
+          <div className="pfpboi">
             <img
               alt=""
               src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fetchfind.com%2Fblog%2Fwp-content%2Fuploads%2F2017%2F08%2Fcat-2734999_1920-5-common-cat-sounds.jpg&f=1&nofb=1"
